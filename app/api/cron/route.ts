563cd4db75bcdd8a1e46d9e4e91bafc943b73f0b
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2024-11-22 11:53:58 +0530

    fix

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -22,51 +22,58 @@ export const GET = async () => {
 
     if (!products.length) throw new Error("No products found");
 
-    const updatedProducts = await Promise.all([
-      products.map(async (product) => {
-        const scrapedProduct = await scrapeAmazonProduct(product.url);
+    const promises = products.map(async (product) => {
+      const scrapedProduct = await scrapeAmazonProduct(product.url);
 
-        if (!scrapedProduct) throw new Error("No product found");
+      if (!scrapedProduct) throw new Error("No product found");
 
-        const updatedPriceHistory = [
-          ...product.priceHistory,
-          { price: scrapedProduct.currentPrice, date: new Date() },
-        ];
+      const updatedPriceHistory = [
+        ...product.priceHistory,
+        { price: scrapedProduct.currentPrice, date: new Date() },
+      ];
 
-        const newProduct = {
-          ...scrapedProduct,
-          priceHistory: updatedPriceHistory,
-          lowestPrice: getLowestPrice(updatedPriceHistory),
-          averagePrice: getAveragePrice(updatedPriceHistory),
-          highestPrice: getHighestPrice(updatedPriceHistory),
+      const newProduct = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+      };
+
+      const updatedProduct = await Product.findOneAndUpdate(
+        { url: product.url },
+        newProduct
+      ).lean();
+
+      const emailNotifType = getEmailNotifType(scrapedProduct, product);
+
+      console.log({
+        emailNotifType,
+        users: updatedProduct?.users,
+        scrapedProduct,
+      });
+
+      if (emailNotifType && updatedProduct?.users.length) {
+        console.log("E");
+        const productInfo = {
+          title: updatedProduct.title,
+          url: updatedProduct.url,
+          image: updatedProduct.image,
         };
 
-        const updatedProduct = await Product.findOneAndUpdate(
-          { url: product.url },
-          newProduct
-        ).lean();
+        const emailContent = await generateEmailBody(
+          productInfo,
+          emailNotifType
+        );
 
-        const emailNotifType = getEmailNotifType(scrapedProduct, product);
+        const userEmails = updatedProduct.users.map((user) => user.email);
 
-        if (emailNotifType && updatedProduct?.users.length) {
-          const productInfo = {
-            title: updatedProduct.title,
-            url: updatedProduct.url,
-            image: updatedProduct.image,
-          };
+        await sendEmail(emailContent, userEmails);
+      }
+      return updatedProduct;
+    });
 
-          const emailContent = await generateEmailBody(
-            productInfo,
-            emailNotifType
-          );
-
-          const userEmails = updatedProduct.users.map((user) => user.email);
-
-          await sendEmail(emailContent, userEmails);
-        }
-        return updatedProduct;
-      }),
-    ]);
+    const updatedProducts = await Promise.all(promises);
 
     return NextResponse.json({ message: "OK", data: updatedProducts });
   } catch (error: any) {
